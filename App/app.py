c952615497c4f883d29455567189a1a794987361
dragos1993 <vrinceanu.dragos24@yahoo.com>
2025-08-13 08:47:20 +0300

    7

--- App/app.py
+++ App/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from AKS via ArgoCD!"
+    return "Hello20"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
